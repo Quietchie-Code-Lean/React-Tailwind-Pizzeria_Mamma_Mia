64900d1c498eb014b32e2707cbd188200948e532
Leandro Funes <leofunesar@gmail.com>
2026-02-14 23:48:50 +0000

    Refactor Done
    Refactor was successfull, import done, dynamic render done, dynamic ingredients render done, I have added description, and insert it within a paragraph,  it was only add props inside the destructuring inside component Cardpizza.jsx, modify the value of the props that I've supplied from the parent component pizzaHome.jsx by mapping and calling every propiety with pizza.propiety

--- src/components/cardPizza.jsx
+++ src/components/cardPizza.jsx
@@ -1,21 +1,15 @@
 
-const CardPizza = ({ idName, price, ingredients, img }) => {
+const CardPizza = ({ name, price, ingredients, img, description }) => {
 
     /* Preset styles */
-    /* Preset styles */
-
-const cardClass = "bg-white rounded-lg shadow-md overflow-hidden";
-const imageClass = "w-full h-48 object-cover";
-const bodyClass = "p-4";
-const titleClass = "text-lg font-semibold mb-2 text-slate-600";
-const ingredientsClass = "text-sm text-gray-600 mb-4 border-y-2 py-4";
-const footerClass = "flex justify-between items-center pb-4";
-const buttonClass = "bg-sky-500 text-white px-4 py-1 rounded hover:bg-sky-600 transition";
-const priceClass = "text-lg font-bold text-sky-300";
-
-
-
-
+    const cardClass = "bg-white rounded-lg shadow-md overflow-hidden";
+    const imageClass = "w-full h-48 object-cover";
+    const bodyClass = "p-4";
+    const titleClass = "text-lg font-semibold mb-2 text-slate-600";
+    const ingredientsClass = "text-sm text-gray-600 mb-4 border-y-2 py-4";
+    const footerClass = "flex justify-between items-center pb-4";
+    const buttonClass = "bg-sky-500 text-white px-4 py-1 rounded hover:bg-sky-600 transition";
+    const priceClass = "text-lg font-bold text-sky-300";
 
 
     return(
@@ -23,20 +17,27 @@ const priceClass = "text-lg font-bold text-sky-300";
         
                 <img
                     src={img}
-                    alt={idName}
+                    alt={name}
                     className={imageClass}/>
 
                 <div className={bodyClass}>
-                    <h3 className={titleClass}>Pizza: {idName}</h3>
 
-                    <p className={ingredientsClass}>Ingredients: <br />{ingredients.join(", ")}</p>
+                    <h3 className={titleClass}>{name.toUpperCase()}</h3>
+
+                    <p>{description}</p>
+
+                    <p className={ingredientsClass}>Ingredients:
+                        <br />
+                        <ul id="renderIngredients">
+                            {ingredients.map(ingredient => <li>{ingredient}</li>)}
+                        </ul>
+                    </p>
+                
 
                     <div className={footerClass}>
                         <button className={buttonClass}>Add to cart
                         </button>
-
                         <span className={priceClass}>${price}</span>
-
                     </div>
 
                 </div>
--- src/layout/PizzasHome.jsx
+++ src/layout/PizzasHome.jsx
@@ -1,5 +1,6 @@
 import CardPizza from "../components/cardPizza";
 import HeaderPizza from "../components/headerPizza";
+import { pizzas } from "../../pizzas";
 
 
 const PizzasHome = () => {
@@ -18,45 +19,16 @@ const PizzasHome = () => {
     <section className={sectionClass}>
       <h2 className={titleClass}>Check our products:</h2>
           <div className={gridClass}>
-
-              
-
+              {pizzas.map(pizza => (
               <CardPizza
-                  idName="Napolitana"
-                  price={5980}
-                  ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-                  img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"/>
-            
-              <CardPizza
-                idName="Española"
-                price={6950}
-                ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"/>
-              
-              <CardPizza
-                idName="Pepperoni"
-                price={6950}
-                ingredients={["mozzarella", "pepperoni", "orégano"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"/>
-
-              <CardPizza
-                  idName="Napolitana"
-                  price={5980}
-                  ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-                  img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"/>
-            
-              <CardPizza
-                idName="Española"
-                price={6950}
-                ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"/>
-              
-              <CardPizza
-                idName="Pepperoni"
-                price={6950}
-                ingredients={["mozzarella", "pepperoni", "orégano"]}
-                img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"/>
-
+                  key={pizza.id}
+                  id={pizza.id}
+                  name={pizza.name}
+                  desc={pizza.desc}
+                  price={pizza.price}
+                  ingredients={pizza.ingredients}
+                  img={pizza.img}/>
+            ))}
           </div>
     </section>
     </>
