325958c391f22897ac1f419ab4bda06af187bd48
Leandro Funes <leofunesar@gmail.com>
2026-03-19 13:51:56 +0000

    feat(Navbar): Refactor within styles and logic componenet NavbarPizza.jsx
    Changes:
    - style: Add preset class Tailwind styles to clean code inline tags.
    - style: Added ActiveClass, InacticeClass and LinkBaseClass.
    - feat: Added isActive class within Navlinks to add styles to active links.
    - feat: Created getNavLinkClass function to controle is active class.
            The function controle and render base style plus state style.

--- src/layout/navbarPizza.jsx
+++ src/layout/navbarPizza.jsx
@@ -5,29 +5,44 @@ import { CartContext } from "../context/CartContext";
 
 const NavbarPizza = () => {
 
+    /* Preset Tailwind Styles */
+const navClass = "w-full bg-slate-800 text-white";
+const wrapperClass = "px-6";
+const innerClass = "flex h-16 items-center justify-between";
+const leftGroupClass = "flex gap-4 items-center";
+const logoClass = "text-xs font-semibold";
+const linksContainerClass = "flex gap-4 text-sm";
+const linkBaseClass = "px-2 py-1 rounded-md transition-colors duration-200";
+const activeClass = "bg-slate-700 text-white";
+const inactiveClass = "text-slate-300 hover:text-white hover:bg-slate-700/50";
+const cartClass = "text-sm font-semibold bg-slate-700 px-3 py-1 rounded-md hover:bg-slate-600 transition";
+
+
     /* This line activate useContext within Navbar component and bring data from CartContext */
+    const getNavLinkClass = ({isActive}) => 
+        `${linkBaseClass} ${isActive ? {activeClass} : {inactiveClass}}`;
+
     const { total } = useContext(CartContext);
     const token = false;
   
     return (
 <>
-        <nav className="w-full bg-slate-800 text-white">
-            <div className="px-6">
-                <div className="flex h-16 items-center justify-between">
+        <nav className={navClass}>
+            <div className={wrapperClass}>
+                <div className={innerClass}>
                 
-
-                    <div className="flex gap-4 items-center">
-                        <h1 className="text-xs font-semibold">Mamma Mía</h1>
+                    <div className={leftGroupClass}>
+                        <h1 className={logoClass}>Mamma Mía</h1>
                 
-                        <div className="flex gap-4 text-sm">
-                            <NavLink to="/">Home</NavLink>
-                            <NavLink to="/register">Register</NavLink>
-                            <NavLink to="/login">Login</NavLink>
-                            <NavLink to="/profile">Profile</NavLink>
+                        <div className={linksContainerClass}>
+                            <NavLink to="/" className={getNavLinkClass}>Home</NavLink>
+                            <NavLink to="/register" className={getNavLinkClass}>Register</NavLink>
+                            <NavLink to="/login" className={getNavLinkClass}>Login</NavLink>
+                            <NavLink to="/profile" className={getNavLinkClass}>Profile</NavLink>
                         </div>
                     </div>
 
-                    <NavLink to="/cart">🛒 Total: ${total}</NavLink>
+                    <NavLink to="/cart" className={cartClass}>🛒 Total: ${total}</NavLink>
 
                 </div>
             </div>
